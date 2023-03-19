6ece4c293be7a4eb7e655dedece4dbecf962abce
Angel F. R <128071948+roscaman@users.noreply.github.com>
2023-03-19 18:53:21 +0100

    Update if_statement.sh

--- if_statement.sh
+++ if_statement.sh
@@ -7,3 +7,19 @@ then
 else
     echo "The file do not."
 fi
+
+
+# Another one, simple Bash script.
+#!/bin/bash
+
+commmand=/usr/bin/tree
+
+if [ -f $command ]
+then
+    echo "$command is available, let's run it..."
+else
+    echo "$command is NOT."
+    sudo apt update && sudo apt upgrade -y tree
+fi
+$commmand
+
